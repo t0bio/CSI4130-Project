67adf0b4137bd3a004c36c68bf211b180c4ea38d
Angeleeca Jocson <angeleeca.jocson@gmail.com>
2024-04-03 21:43:09 -0400

    slay

--- LogHut.js
+++ LogHut.js
@@ -5,26 +5,27 @@ class LogHut {
     constructor(scene) {
         this.scene = scene;
         this.loghut = new THREE.Group();
-        this.scene.add(this.loghut);
         this.loadModel();
     }
 
     loadModel() {
-        const loader = new FBXLoader();
-        loader.load('models/LogHut.fbx', (object) => {
-            this.loghut = object;
-            this.loghut.scale.set(0.005, 0.005, 0.005);
-            this.scene.add(this.loghut);
-        }, undefined, function (error) {
-            console.error(error);
+        return new Promise((resolve, reject) => {
+            const loader = new FBXLoader();
+            loader.load('models/MyLogHut.fbx', (object) => {
+                object.scale.set(0.005, 0.005, 0.005);
+                this.loghut.add(object); // Assuming this.loghut is a THREE.Group
+                this.scene.add(this.loghut);
+                resolve(); // Resolve the promise
+            }, undefined, function (error) {
+                console.error(error);
+                reject(error); // Reject the promise
+            });
         });
-        this.setPosition(0, 0, 0); // Initialize position at the origin
-        this.addToScene();
     }
     
     addToScene() {
-        if (this.scene) {
-            this.scene.add(this.tree);
+        if (this.scene && this.loghut) {
+            this.scene.add(this.loghut);
         }
     }
     
